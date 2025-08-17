847b0538504718a8c3a4f1029f9799a508b8ac4d
learninjavadotcom <learninjavadotcom@gmail.com>
2025-08-17 02:03:55 -0700

    fix: Fixed the run script

--- run_server.sh
+++ run_server.sh
@@ -3,5 +3,6 @@
 # Run the application
 echo "Starting FastAPI application..."
 echo "Access the API at http://localhost:8000"
-source venv/bin/activate
+uv venv .venv
+source .venv/bin/activate
 nohup uvicorn main:app --host 0.0.0.0 --port 8000 --reload > uvicorn.log 2>&1 &
